f9ddec1e067c0d97a84a33d5348457669d318649
Christian Esparza <cesparzajp@gmail.com>
2024-08-29 16:21:49 +0900

    Set page layout

--- app.js
+++ app.js
@@ -11,6 +11,8 @@ app.use(express.urlencoded({extended: true}));
 
 //Set the root directory of the templates in views
 app.set("views",path.join(__dirname, "views"));
+//Enable layout support through extend('layout')
+app.engine('ejs',require('express-ejs-extend'));
 //Enable EJS as view engine
 app.set("view engine", "ejs");
 
@@ -19,6 +21,39 @@ const assetsPath = path.join(__dirname, "public");
 app.use(express.static(assetsPath));
 
 //Render the views
+//Header
+//Logo
+const mainLogo = {
+    src: "/images/the-dance-thread-logo-dark.png",
+    alt: "The Dance Thread"
+};
+
+//Navigation links
+const navLinks = [
+    {href: "/", text: "Home" },
+    {href: "events", text: "Events"},
+    {href: "artists", text: "Artists"},
+    {href: "djs", text: "DJs"},
+    {href: "about", text: "About" },
+    {href: "contact", text: "Contact"},
+];
+
+//Github link
+const profileLink = {
+    href: "https://github.com/Durounseki",
+    aria: "GitHub profile of Durounseki",
+    faClass: "fa-brands fa-github",
+    text: "Durounseki"
+}
+
+app.use( (req,res,next) => {
+    res.locals.mainLogo = mainLogo;
+    res.locals.navLinks = navLinks;
+    res.locals.profileLink = profileLink;
+    next();
+});
+
+
 //home
 app.use("/",indexRouter);
 

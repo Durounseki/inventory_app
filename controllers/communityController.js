62e7383c381b9dbbe857e6eb9d8b08e489889b14
Christian Esparza <cesparzajp@gmail.com>
2024-10-07 23:15:47 +0900

    refactor: profile page

--- controllers/communityController.js
+++ controllers/communityController.js
@@ -77,13 +77,161 @@ const getVerification = [
     }
 ]
 
-const getProfile = [
-    // ensureAuthenticated,
-    async (req,res,next) => {
-        const user = req.user;
-        res.render('profile', {title: "Profile", user: user, script: "profile.js"});
+// const getProfile = [
+//     // ensureAuthenticated,
+//     async (req,res,next) => {
+//         // const user = req.user || false;
+//         res.render('profile', {title: "Profile", user: user, script: "profile.js"});
+//     }
+// ]
+
+const ownUserActions = [
+    {
+        name: "Edit Profile",
+        href: "/community/profile/edit"
+    },
+    {
+        name: "Settings",
+        href: "/community/profile/settings"
     }
 ]
+
+const otherUserActions = [
+    {
+        name: "Danced",
+        href: "/community/profile/danced"
+    },
+    {
+        name: "Want to Dance",
+        href: "/community/profile/want-to-dance"
+    }
+]
+
+const ownInfoTabs = [
+    {
+        name: "About",
+        href: "/community/profile/about"
+    },
+    {
+        name: "Events",
+        href: "/community/profile/events"
+    }
+]
+
+const othersInfoTabs = [
+    {
+        name: "Danced",
+        href: "/community/profile/danced"
+    },
+    {
+        name: "Want to Dance",
+        href: "/community/profile/want-to-dance"
+    }
+]
+
+const authorized = true;
+
+const user = {
+    name: "Christian",
+    greeting: "Hey! I'm the developer of the dance thread!",
+    danced: 10,
+    wantToDance: 3000,
+    bio: `
+        Hey there, this is Christian, the developer of this website. I hope you are enjoying your experience using The Dance Thread! Please don't hesitate to contact me to ask questions, make some suggestions, or simply say hello.
+        
+        Let me tell you a little bit about me, and the story behind The Dance Thread. I'm originally from Mexico, and even though dancing is a big part of our culture, I never dared to learn while living there. I always thought I was too clumsy and felt insecure thinking people would laugh at me. My dance journey actually started when I moved to the UK for my studies. I learned the basics of salsa and bachata there and discovered how beautiful it is to connect with someone through dance. I was instantly hooked! But I still didn't feel confident enough to go out to socials, not to mention that I was extremely busy trying to finish my PhD in the middle of the pandemic back in 2020.
+                
+        It wasn't until I came to Okinawa that I really started learning properly, first at Mamboki with Erik Rodriguez and later with Andres y Yuno at Haisai Latina. It took months to build the skills and confidence to ask people to dance. You know how hard it is to step onto the dance floor when you can only do the basic step! But, if you're like me, you also know how fun and addictive dancing can be. There's something magical about losing yourself in the music and movement, spending hours at a dance club without even noticing the time fly by.  You create these strong bonds with people simply by connecting on the dance floor, regardless of what language you speak. It's amazing how dance can create friendships anywhere in the world where there's a welcoming community.
+                
+        And speaking of community, it was here in Okinawa that I met Yazmin, who sparked the idea for The Dance Thread. She envisioned an inclusive space for all dancers, which instantly resonated with how I feel about dancing. Coincidentally, at the time, I was looking to start a career in web development. It felt like the perfect partnership! We've been working hard for the last few months and have come up with this initial product. There's still tons of work to do, but we're working relentlessly to connect dancers from all around the world and spread our passion to many more.
+                
+        We're working hard to bring you more features soon, including learning resources and opportunities to share your work as dancers, connecting both personally and professionally.
+        
+        Start exploring The Dance Thread today!
+    `
+}
+
+const getProfileAbout = [ async(req,res,next) => {
+    
+    res.render('profile', {
+        title: 'Profile',
+        user: user,
+        authorized: authorized,
+        userActions: authorized ? ownUserActions : otherUserActions,
+        userTabs: ownInfoTabs.map(tab => 
+            tab.name === "About"
+            ? {...tab, class: "details-tab active"}
+            : {...tab, class: "details-tab"}
+        ),
+        about: true,
+        events: false,
+        community: false  
+    })
+}]
+const getProfileEvents = [ async(req,res,next) => {
+    res.render('profile', {
+        title: 'Profile',
+        user: user,
+        authorized: authorized,
+        userActions: authorized ? ownUserActions : otherUserActions,
+        userTabs: ownInfoTabs.map(tab => 
+            tab.name === "Events"
+            ? {...tab, class: "details-tab active"}
+            : {...tab, class: "details-tab"}
+        ),
+        about: false,
+        events: true,
+        community: false
+    })
+}]
+
+const getProfileEdit = [ async(req,res,next) => {
+    res.render('settings', {
+        title: 'Edit Profile',
+        user: user,
+    })
+}]
+
+const getProfileSettings = [ async(req,res,next) => {
+    res.render('settings', {
+        title: 'Settings',
+        user: user
+    })
+}]
+
+const getProfileDanced = [ async(req,res,next) => {
+    res.render('profile', {
+        title: 'Profile',
+        user: user,
+        authorized: authorized,
+        userActions: authorized ? ownUserActions : otherUserActions,
+        userTabs: othersInfoTabs.map(tab => 
+            tab.name === "Danced"
+            ? {...tab, class: "details-tab active"}
+            : {...tab, class: "details-tab"}
+        ),
+        about: false,
+        events: false,
+        community: true
+      })
+}]
+
+const getProfileWantToDance = [ async(req,res,next) => {
+    res.render('profile', {
+        title: 'Profile',
+        user: user,
+        authorized: authorized,
+        userActions: authorized ? ownUserActions : otherUserActions,
+        userTabs: othersInfoTabs.map(tab => 
+            tab.name === "Want to Dance"
+            ? {...tab, class: "details-tab active"}
+            : {...tab, class: "details-tab"}
+        ),
+        about: false,
+        events: false,
+        community: true
+    })
+}]
 
 const communityController = {
     showDashboard,
@@ -96,7 +244,13 @@ const communityController = {
     getSignup,
     postSignup,
     getVerification,
-    getProfile
+    getProfileAbout,
+    getProfileEvents,
+    getProfileDanced,
+    getProfileWantToDance,
+    getProfileEdit,
+    getProfileSettings,
+    // getProfile
 };
 
 export default communityController;
--- routes/communityRouter.js
+++ routes/communityRouter.js
@@ -21,6 +21,12 @@ communityRouter.get("/reset-password/:tokenId", communityController.getResetPass
 communityRouter.get("/reset-password", communityController.getResetPassword);
 communityRouter.post("/reset-password", communityController.postResetPassword);
 //Render profile page
-communityRouter.get("/profile", communityController.getProfile);
+communityRouter.get("/profile/about", communityController.getProfileAbout);
+communityRouter.get("/profile/events", communityController.getProfileEvents);
+communityRouter.get("/profile/edit", communityController.getProfileEdit);
+communityRouter.get("/profile/settings", communityController.getProfileSettings);
+communityRouter.get("/profile/danced", communityController.getProfileDanced);
+communityRouter.get("/profile/want-to-dance", communityController.getProfileWantToDance);
+
 
 export default communityRouter;

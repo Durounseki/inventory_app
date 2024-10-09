6fe99315e94057c3b7a61dc2ad61d1f19291573c
Christian Esparza <cesparzajp@gmail.com>
2024-10-09 20:45:08 +0900

    feat: add profile settings page

--- controllers/communityController.js
+++ controllers/communityController.js
@@ -163,24 +163,30 @@ const user = {
     danced: 10,
     wantToDance: 3000,
     bio: `
-        Hey there, this is Christian, the developer of this website. I hope you are enjoying your experience using The Dance Thread! Please don't hesitate to contact me to ask questions, make some suggestions, or simply say hello.
+    Hey there, this is Christian, the developer of this website. I hope you are enjoying your experience using The Dance Thread! Please don't hesitate to contact me to ask questions, make some suggestions, or simply say hello.
         
-        Let me tell you a little bit about me, and the story behind The Dance Thread. I'm originally from Mexico, and even though dancing is a big part of our culture, I never dared to learn while living there. I always thought I was too clumsy and felt insecure thinking people would laugh at me. My dance journey actually started when I moved to the UK for my studies. I learned the basics of salsa and bachata there and discovered how beautiful it is to connect with someone through dance. I was instantly hooked! But I still didn't feel confident enough to go out to socials, not to mention that I was extremely busy trying to finish my PhD in the middle of the pandemic back in 2020.
+    Let me tell you a little bit about me, and the story behind The Dance Thread. I'm originally from Mexico, and even though dancing is a big part of our culture, I never dared to learn while living there. I always thought I was too clumsy and felt insecure thinking people would laugh at me. My dance journey actually started when I moved to the UK for my studies. I learned the basics of salsa and bachata there and discovered how beautiful it is to connect with someone through dance. I was instantly hooked! But I still didn't feel confident enough to go out to socials, not to mention that I was extremely busy trying to finish my PhD in the middle of the pandemic back in 2020.
                 
-        It wasn't until I came to Okinawa that I really started learning properly, first at Mamboki with Erik Rodriguez and later with Andres y Yuno at Haisai Latina. It took months to build the skills and confidence to ask people to dance. You know how hard it is to step onto the dance floor when you can only do the basic step! But, if you're like me, you also know how fun and addictive dancing can be. There's something magical about losing yourself in the music and movement, spending hours at a dance club without even noticing the time fly by.  You create these strong bonds with people simply by connecting on the dance floor, regardless of what language you speak. It's amazing how dance can create friendships anywhere in the world where there's a welcoming community.
+    It wasn't until I came to Okinawa that I really started learning properly, first at Mamboki with Erik Rodriguez and later with Andres y Yuno at Haisai Latina. It took months to build the skills and confidence to ask people to dance. You know how hard it is to step onto the dance floor when you can only do the basic step! But, if you're like me, you also know how fun and addictive dancing can be. There's something magical about losing yourself in the music and movement, spending hours at a dance club without even noticing the time fly by.  You create these strong bonds with people simply by connecting on the dance floor, regardless of what language you speak. It's amazing how dance can create friendships anywhere in the world where there's a welcoming community.
                 
-        And speaking of community, it was here in Okinawa that I met Yazmin, who sparked the idea for The Dance Thread. She envisioned an inclusive space for all dancers, which instantly resonated with how I feel about dancing. Coincidentally, at the time, I was looking to start a career in web development. It felt like the perfect partnership! We've been working hard for the last few months and have come up with this initial product. There's still tons of work to do, but we're working relentlessly to connect dancers from all around the world and spread our passion to many more.
+    And speaking of community, it was here in Okinawa that I met Yazmin, who sparked the idea for The Dance Thread. She envisioned an inclusive space for all dancers, which instantly resonated with how I feel about dancing. Coincidentally, at the time, I was looking to start a career in web development. It felt like the perfect partnership! We've been working hard for the last few months and have come up with this initial product. There's still tons of work to do, but we're working relentlessly to connect dancers from all around the world and spread our passion to many more.
                 
-        We're working hard to bring you more features soon, including learning resources and opportunities to share your work as dancers, connecting both personally and professionally.
+    We're working hard to bring you more features soon, including learning resources and opportunities to share your work as dancers, connecting both personally and professionally.
         
-        Start exploring The Dance Thread today!
+    Start exploring The Dance Thread today!
     `,
     style: ['Salsa', 'Bachata', 'Cumbia'],
     sns: [{
         name: "instagram",
         url: "https://www.instagram.com/durounseki/profilecard/?igsh=MWdvd3RlaXNxbnBlNw==",
         faClass: "fa-brands fa-instagram"
-    }]
+    }],
+    preferences: {
+        visibility: {
+            network: "anyone",
+            events: "anyone"
+        }
+    }
 
 
 }
@@ -228,7 +234,10 @@ const getProfileEdit = [ async(req,res,next) => {
             tab.name === "Edit Profile"
             ? {...tab, class: "details-tab active"}
             : {...tab, class: "details-tab"}
-        )
+        ),
+        editProfile: true,
+        profileSettings: false
+
     })
 }]
 
@@ -241,7 +250,9 @@ const getProfileSettings = [ async(req,res,next) => {
             tab.name === "Settings"
             ? {...tab, class: "details-tab active"}
             : {...tab, class: "details-tab"}
-        )
+        ),
+        editProfile: false,
+        profileSettings: true
     })
 }]
 

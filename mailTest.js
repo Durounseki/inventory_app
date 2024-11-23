226af9e2231c1f19ef6c1d31226a244d22595322
Christian Esparza <156300780+Durounseki@users.noreply.github.com>
2024-11-23 23:02:27 +0900

    edit: mailTest

--- mailTest.js
+++ mailTest.js
@@ -8,17 +8,17 @@ const mailerSend = new MailerSend({
 const sentFrom = new Sender(process.env.MAILERSEND_EMAIL, "The Dance Thread");
 
 const recipients = [
-  new Recipient("cesparzadev@gmail.com", "Your Client")
+  new Recipient("cesparzajp@gmail.com", "Your Client")
 ];
 
 const emailParams = new EmailParams()
   .setFrom(sentFrom)
   .setTo(recipients)
-  .setSubject("This is a Subject")
+  .setSubject("Testing MailerSend")
   .setHtml("<strong>This is the HTML content</strong>")
   .setText("This is the text content");
 
 mailerSend.email
 	.send(emailParams)
-  .then((response) => console.log(response))
+  .then((response) => response.statusCode === 202 ? console.log("Email sent") : console.log("Error sending email"))
   .catch((error) => console.log(error));
